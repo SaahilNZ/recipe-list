5946d5b3cdf37f015e6a14afd36493e9a3afbb97
SaahilNZ <saahilh@outlook.co.nz>
2018-04-14 16:54:36 +1200

    Move recipe model into shared library.

--- RecipeList.Shared/Model/Recipe.cs
+++ RecipeList.Shared/Model/Recipe.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace RecipeList.Web
+namespace RecipeList.Model
 {
     public class Recipe
     {
--- RecipeList.Web/Pages/RecipeDetails.cshtml.cs
+++ RecipeList.Web/Pages/RecipeDetails.cshtml.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using RecipeList.Web;
+using RecipeList.Model;
 
 namespace RecipeList.Web.Pages
 {
     public class RecipeDetailsModel : PageModel
     {
-        public Recipe PageRecipe { get; set; }
+        public Recipe Recipe { get; set; }
 
         public void OnGet()
         {
-            PageRecipe = new Recipe
+            Recipe = new Recipe
             {
                 Name = "Spinach and Feta Scrolls",
                 CreatedBy = new User
