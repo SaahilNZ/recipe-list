bc2700ceba32bca3ec7804e4c7e1ca948800e0bc
SaahilNZ <saahilh@outlook.co.nz>
2018-05-03 23:28:44 +1200

    Reorder ingredients and method steps using drag-and-drop. Add minified files to gitignore.

--- RecipeList.Web/static/js/site.js
+++ RecipeList.Web/static/js/site.js
@@ -1,28 +1,65 @@
-﻿function addItemToListGroup(textBoxId, listId, propertyName) {
+﻿$(document).ready(function () {
+    $('.sortable').sortable({
+        placeholderClass: 'list-group-item',
+        handle: '.handle'
+    });
+});
+
+function addItemToListGroup(textBoxId, listId, propertyName) {
     let textBox = $(textBoxId);
     let itemList = $(listId);
 
     let textBoxValue = textBox.val();
     if (textBoxValue != "" ) {
         let item = document.createElement("li");
-        item.className = "list-group-item";
-        item.innerText = textBoxValue;
-    
+        item.className = "list-group-item container";
+        item.draggable = true;
+        
+        let row = document.createElement("div");
+        row.className = "row";
+
+        let handleContainer = document.createElement("div");
+        handleContainer.className = "col-sm-auto handle-container";
+        
+        let handle = document.createElement("div");
+        handle.className = "fas fa-bars handle";
+        handleContainer.appendChild(handle);
+
+        let textContainer = document.createElement("div");
+        textContainer.className = "col-sm-auto text-container";
+        
+        let text = document.createElement("span");
+        text.className = "list-text";
+        text.innerText = textBoxValue;
+        textContainer.appendChild(text);
+
         let inputElement = document.createElement("input");
         inputElement.name = propertyName;
         inputElement.type = "hidden";
         inputElement.value = textBoxValue;
 
-        let deleteButton = document.createElement("span");
+        let deleteContainer = document.createElement("div");
+        deleteContainer.className = "col-sm-auto delete-container";
+
+        let deleteButton = document.createElement("div");
         deleteButton.className = "float-right list-icon far fa-trash-alt";
         deleteButton.addEventListener("click", function () {
             removeItem(this);
         });
-    
+        deleteContainer.appendChild(deleteButton);
+
+        row.appendChild(handleContainer);
+        row.appendChild(textContainer);
+        row.appendChild(deleteContainer);
+        item.appendChild(row);
         item.appendChild(inputElement);
-        item.appendChild(deleteButton);
     
         itemList.append(item);
+
+        $('.sortable').sortable({
+            placeholderClass: 'list-group-item',
+            handle: '.handle'
+        });
     }
 }
 
@@ -35,5 +72,5 @@ function addMethodStep() {
 }
 
 function removeItem(button, onItemRemoved) {
-    button.parentNode.remove();
+    button.parentNode.parentNode.parentNode.remove();
 }
